60c0489736d42f083d72be0928f7ca2be7275816
Walter Martins-Filho <waltersmartinsf@gmail.com>
2018-07-01 17:22:09 -0300

    including license

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='astroscripts',
+      version='0.0.1',
+      description='Personal library to work with astronomical data analysis',
+      url='https://github.com/waltersmartinsf/astroscripts',
+      author='Walter Silva Martins Filho',
+      author_email='walter@on.br',
+      license=' Creative Commons Attribution 4.0 International License',
+      packages=['astroscripts'],
+      zip_safe=False)
