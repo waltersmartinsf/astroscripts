75474e64e6bac97d7af009be5245accc3b5ac873
Walter Martins-Filho <waltersmartinsf@gmail.com>
2018-07-01 19:44:23 -0300

    finishing tree structure for personal python library

--- build/lib/astroscripts/astroscripts.py
+++ build/lib/astroscripts/astroscripts.py
@@ -1,3 +1,7 @@
 # ASTROSCRIPTS
 
 ## This library is a personal library that include helpful functions for my daily work.
+
+def find_files(file_string):
+    from glob import glob
+    return glob(file_string)
